fa789e2799fb9c2c2509f019b487f80cfecb7204
maaront <35611834+maaront@users.noreply.github.com>
2023-05-14 11:27:39 -0400

    build prompt logic to display db via inquirer

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
+const { startPrompt } = require('./index.js');
 
 // Connect to database
 const db = mysql.createConnection(
   {
     host: '127.0.0.1',
-    // MySQL username,mysql -u root
     user: 'root',
-    // MySQL password
     password: 'password',
     database: 'company_db'
   },
   console.log(`Connected to the company_db database.`)
 );
+
